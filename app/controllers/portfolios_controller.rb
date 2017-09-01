a28f7849c93eef92bc6e4bd515764970c85e7cea
JaggerHung <jagger@choxue.com>
2017-09-01 14:57:28 +0800

    finished portfolio destroy method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,4 +38,16 @@ class PortfoliosController < ApplicationController
  		@portfolio_item = Portfolio.find(params[:id])
 	end
 
+	def destroy
+		# Perform the lookup
+		@portfolio_item = Portfolio.find(params[:id])
+		# Destroy the Record
+		@portfolio_item.destroy
+		# Redirect
+		respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: 'Blog was successfully destroyed.' }
+    end
+
+	end
+
 end
